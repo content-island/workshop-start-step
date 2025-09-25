3e4f2b5cbf4d11cd3b1560235acd20e103147b16
Leticia de la Osa <deletidev@gmail.com>
2025-09-25 18:15:02 +0200

    Refactor mini-bio components by removing unused props and types

--- src/pods/mini-bio/mini-bio.model.ts
+++ src/pods/mini-bio/mini-bio.model.ts
@@ -1,10 +1,1 @@
-export interface MiniBio {
-  title: string;
-  name: string;
-  role: string;
-  description: string;
-  imageUrl: string;
-  imageAlt: string;
-}
-
 export type MiniBioType = 'hero' | 'card';
